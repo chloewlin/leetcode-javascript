ad420cb341ef50630faedee7eb47f266db66d37e
Chloe Lin <chloewjlin@gmail.com>
2018-09-20 18:31:16 -0700

    Refactor code to add negative sign

--- 7 - reverseInt.js
+++ 7 - reverseInt.js
@@ -1,9 +1,10 @@
 function reverseInt(num) {
   numStr = num.toString().split('');
-  return num > 0 ? Number(numStr.reverse().join('')) : Number(numStr.slice(1).reverse().join(''));  
+  return num > 0 ? Number(numStr.reverse().join('')) : "-" + Number(numStr.slice(1).reverse().join(''));  
 }
 
 console.log(reverseInt(123)); // 321
 console.log(reverseInt(120)); // 21
 console.log(reverseInt(-123)); // -321
 console.log(reverseInt(12000)); // 21
+console.log(reverseInt(-1234)); // -4321

5984b26b62f7bce26d5bbd0053f96f32de592c46
Chloe Lin <chloewjlin@gmail.com>
2018-09-24 14:55:35 -0700

    Complete longestPalindromeSubstr

---
+++ 5 - longestPalindromeSubstr.js
@@ -0,0 +1,35 @@
+let isPalindrome = function(str) {
+  const originalStr = str.toLowerCase();
+  const reversedStr = str.toLowerCase().split('').reverse().join('');
+  return originalStr === reversedStr ? true : false;
+}
+// console.log(isPalindrome("Hannah"));
+
+let longestPalindrome = function(str) {
+  let palindromes = [];
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 1; j <= str.length; j++) {
+      let substr = str.substr(i, j);
+      let length = str.substr(i, j).length;
+      if (isPalindrome(substr)) {
+        palindromes.push({substr, length});
+      }
+    }
+  }
+  console.log(palindromes);
+  let maxLength = 0;
+  let max = null;
+  for (let i = 0; i < palindromes.length; i++) {
+    if (palindromes[i].length > maxLength) {
+      maxLength = palindromes[i].length;
+      max = palindromes[i].substr;
+    }
+  }
+  console.log(maxLength);
+  console.log(max);
+  return max;
+};
+
+console.log(longestPalindrome("babad")); // "bab"
+console.log(longestPalindrome("cbbd")); // "bb"

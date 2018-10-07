ffda9361ff65461528774e39fb3e218f0da41587
Chloe Lin <chloewjlin@gmail.com>
2018-10-06 23:33:46 -0700

    Complete 563_binaryTreeTilt with recursion and IIFE

--- 563_binaryTreeTilt.js
+++ 563_binaryTreeTilt.js
@@ -14,43 +14,30 @@ function TreeNode(val) {
   this.left = this.right = null;
 };
 
-var helper = (tree) => {
-  // if (!tree.left) tree.left.val = 0; // this is wrong
-  // if (!tree.right) tree.right.val = 0;
-  // terminating case: when the current node does not have children nodes
-  if (tree.left !== null && tree.right !== null) {
-    return Math.abs(tree.left.val - tree.right.val);
-  }
-  // recursive case: if node has children, continue to check the children
-  // if (tree.val && tree.left) {
-  //   helper(tree.left);
-  // }
-  // if (tree.val && tree.right) {
-  //   helper(tree.right);
-  // }
-};
 
 var findTilt = function(root) {
-  let tilts = 0;
-  let sumOfLeft;
-  let sumOfRight 
-  if (root.left) {
-    sumOfLeft = helper(root.left);
-    console.log(helper(root.left));
-  }
-  if (root.right) {
-    sumOfRight = helper(root.right);
-  }
-  tilts = sumOfLeft + sumOfRight;
-  return tilts;
+  let totalTilts = 0;
+  (function nodeSum(node) {
+    let leftSum = 0;
+    let rightSum = 0;
+    if (node.left) {
+      leftSum = nodeSum(node.left);
+    }
+    if (node.right) {
+      rightSum = nodeSum(node.right);
+    }
+    totalTilts += Math.abs(leftSum - rightSum);
+    return leftSum + rightSum + node.val;
+  })(root);
+  return totalTilts;
 };
 
 var test = new TreeNode(1);
 test.left = new TreeNode(2);
 test.right = new TreeNode(3);
-// test.left.left = new TreeNode(4);
-// test.left.right = new TreeNode(5);
-// test.right.right = new TreeNode(7);
+test.left.left = new TreeNode(4);
+test.left.right = new TreeNode(5);
+test.right.left = new TreeNode(6);
+test.left.left.left = new TreeNode(7);
 console.log(test);
-console.log('helper', helper(test));
 console.log(findTilt(test));

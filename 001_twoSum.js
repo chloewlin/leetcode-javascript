53f081fc823a6c201e158c4b4ad7ea98e4a95a33
Chloe Lin <chloewjlin@gmail.com>
2018-10-07 23:52:14 -0700

    Add O(n2) solution to 001_twoSum

--- 001_twoSum.js
+++ 001_twoSum.js
@@ -1,4 +1,4 @@
-// Best solution: Time Complexity: O(n)
+// Time Complexity: O(n)
 function twoSum(nums, target) {
   let hashmap = {};
   for (let index in nums) {
@@ -9,4 +9,15 @@ function twoSum(nums, target) {
       return [+index, +hashmap[difference]];
     }
   }
+};
+
+// Time Complexity: O(n2)
+function twoSum(nums, target) {
+  let obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      obj[[nums[i] + nums[j]]] = [i, j];
+    }
+  }
+  return obj[target];
 };

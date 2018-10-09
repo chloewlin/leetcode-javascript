7df9ac3daa65679e5c089165bc02fabc6cd4233c
Chloe Lin <chloewjlin@gmail.com>
2018-10-08 22:51:45 -0700

    Complete 007_reverseInt and pass all tests

--- 007_reverseInt.js
+++ 007_reverseInt.js
@@ -1,14 +1,27 @@
-function reverseInt(num) {
-  numStr = num.toString().split('');
-  if (num > 0) {
-    return Number(numStr.reverse().join(''))
-  } else { 
-    return parseInt("-" + Number(numStr.slice(1).reverse().join('')));  
+var reverse = function(x) {
+  let numStr = x.toString().split('');
+  let num = 0;
+  
+  if (Math.abs(x) < Math.pow(2, 31)) {
+    if (x > 0) {
+      num = +numStr.reverse().join('');
+    } else { 
+      num = parseInt("-" + numStr.slice(1).reverse().join(''));  
+    } 
   } 
-}
+  
+  if (Math.abs(num) > Math.pow(2, 31)) {
+    num = 0;
+  }
+  
+  if (x === 0) return 0;
+  return num; 
+};
 
 console.log(reverseInt(123)); // 321
 console.log(reverseInt(120)); // 21
 console.log(reverseInt(-123)); // -321
 console.log(reverseInt(12000)); // 21
 console.log(reverseInt(-1234)); // -4321
+console.log(reverseInt(0));
+console.log(reverseInt(1534236469)); // Should be 0

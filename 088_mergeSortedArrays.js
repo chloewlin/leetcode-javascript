6b104d02261fe3bd0233c7efbbb45f5f2d0be519
Chloe Lin <chloewjlin@gmail.com>
2018-10-12 23:42:35 -0700

    Fix the breaking condition

--- 088_mergeSortedArrays.js
+++ 088_mergeSortedArrays.js
@@ -5,13 +5,12 @@ var nums2 = [2,5,6];
 var merge = function(nums1, m, nums2, n) {
   var length = m + n - 1; 
   while (length) {
+    if (n === 0) break;
     if (nums1[m-1] > nums2[n-1]) {
       nums1[length] = nums1[m-1];
-      console.log(nums1[m-1])
       m--;
     } else {
       nums1[length] = nums2[n-1];
-      console.log(nums2[n-1])
       n--;
     }
     length--;
@@ -19,3 +18,4 @@ var merge = function(nums1, m, nums2, n) {
   console.log(nums1);
 };
 console.log(merge(nums1, 3, nums2, 3)); //[1,2,2,3,5,6]
+console.log(merge([0], 0, [1], 1));

b9d895391ca88dbdfe362e7870258b51a9015932
Chloe Lin <chloewjlin@gmail.com>
2018-10-14 15:47:45 -0700

    Complete minDepthOfBinaryTree with recursion

---
+++ 111_minDepthOfBinaryTree.js
@@ -0,0 +1,6 @@
+var minDepth = function(root) {
+  if (!root) return 0;
+  if (!root.left) return 1 + minDepth(root.right);
+  if (!root.right) return 1 + minDepth(root.left);
+  return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+};

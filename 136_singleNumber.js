51537c20c68f9da354826056ac6ee77dc12c9b11
Chloe Lin <chloewjlin@gmail.com>
2018-10-14 17:52:01 -0700

    Refactor with arrow func and annotate time/space complexity

--- 136_singleNumber.js
+++ 136_singleNumber.js
@@ -1,10 +1,8 @@
-// Time Complexity: O(1)
-// Space Complexity: S(1)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 // Strategy: XOR (bit manipulation)
 var singleNumber = function(nums) {
-  return nums.reduce(function(acc, currVal){
-    return acc^currVal;
-  });
+  return nums.reduce((a, b) => a^b);
 };
 
 var arr = [1, 2, 2, 3, 3, 5, 9, 8, 7, 5, 7, 8, 9, 1, 4];

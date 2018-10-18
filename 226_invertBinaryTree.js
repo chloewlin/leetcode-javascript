7ce714cae67a6bb1950a96f950aea23b42d29c9c
Chloe Lin <chloewjlin@gmail.com>
2018-10-18 00:43:24 -0700

    Add unbalanced tree to test cases

--- 226_invertBinaryTree.js
+++ 226_invertBinaryTree.js
@@ -1,8 +1,26 @@
+// Invert a binary tree.
+// Example:
+
+// Input:
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+
+// Output:
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+
 function TreeNode(val) {
   this.val = val;
   this.left = this.right = null;
 };
 
+// Time Complexity: O(n)
 var swap = function(root) {
   var temp = root.left;
   root.left = root.right;
@@ -21,6 +39,7 @@ var invertTree = function(root) {
   return root;
 };
 
+// Test case 1: balanced tree 
 const tree = new TreeNode(4);
 tree.left = new TreeNode(2);
 tree.right = new TreeNode(7);
@@ -29,3 +48,29 @@ tree.left.right = new TreeNode(3);
 tree.right.left = new TreeNode(6);
 tree.right.right = new TreeNode(9);
 console.log(invertTree(tree));
+
+// Test case 2: unbalanced tree
+// Input:
+//      4
+//        \
+//         7
+//        / \
+//       6   9
+
+// Output:
+//      4
+//    /   
+//   7     
+//  / \   
+// 9   6 
+
+const unbalancedTree = { 
+  value: 4, 
+  left: null, 
+  right: { 
+    value: 7,
+    left: { value: 6 }, 
+    right: { value: 9 }
+  }
+}
+console.log(invertTree(unbalancedTree));

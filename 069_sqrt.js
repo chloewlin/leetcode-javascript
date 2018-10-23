e4480bf4c78191aaf6257a7ce177971632ee69c6
Chloe Lin <chloewjlin@gmail.com>
2018-10-22 21:59:48 -0700

    Complete sqrt using divide-and-conquer approach

--- 069_sqrt.js
+++ 069_sqrt.js
@@ -1,4 +1,22 @@
+// Divide-and-conquer approach
+// Time Complexity: O(logN)
 var mySqrt = function(x) {
+  var left = 1; 
+  var right = Math.ceil(x/2);
+  var mid;
+
+  while (left <= right) {
+    mid = Math.ceil((left + right) / 2);
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
 };
 
 console.log(mySqrt(4)); // 2

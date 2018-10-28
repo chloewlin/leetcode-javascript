ee449a4e3392158b0702af9a6f5bb607e88d92cc
Chloe Lin <chloewjlin@gmail.com>
2018-10-27 22:40:59 -0700

    Add recursive solution

--- 112_pathSum.js
+++ 112_pathSum.js
@@ -4,23 +4,30 @@ function TreeNode(val) {
 };
 
 // Time Complexity: O(logN)
-var hasPathSum = function(root, target) {
+// var hasPathSum = function(root, target) {
+//   if (!root) return false;
+//   function helper(root, currSum) {
+//     currSum = currSum + root.val;
+//     if (!root.left && !root.right) {
+//       return currSum === target;
+//     }
+//     if (root.left) {
+//       if (helper(root.left, currSum)) return true;
+//     }
+//     if (root.right) {
+//       return helper(root.right, currSum);
+//     }
+//     return false;
+//   };
+//   return helper(root, 0);
+// };
+
+// Time Complexity: O(logN)
+var hasPathSum = function(root, sum) {
   if (!root) return false;
-  function helper(root, currSum) {
-    currSum = currSum + root.val;
-    if (!root.left && !root.right) {
-      return currSum === target;
-    }
-    if (root.left) {
-      if (helper(root.left, currSum)) return true;
-    }
-    if (root.right) {
-      return helper(root.right, currSum);
-    }
-    return false;
-  };
-  return helper(root, 0);
-};
+  if (!root.right && !root.left && root.val === sum) return true;
+  return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+}
 
 var node = new TreeNode(5);
 node.left = new TreeNode(4);
@@ -32,6 +39,6 @@ node.right.left = new TreeNode(13);
 node.right.right = new TreeNode(4);
 node.right.right.right = new TreeNode(1);
 // console.log(node);
-console.log(hasPathSum(node, 22));
+console.log(hasPathSum(node, 22)); // true
 var node1 = new TreeNode(1);
-console.log(hasPathSum(node1, 0));
+console.log(hasPathSum(node1, 0)); // false

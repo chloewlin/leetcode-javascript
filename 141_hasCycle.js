33a8c44442a711335bc26c19d304be0e541c9dee
Chloe Lin <chloewjlin@gmail.com>
2018-10-30 21:41:32 -0700

    Add solution and fix bug in the terminating condition in while loop
    When using two pointers, we should always check if fast.next is not
    null to avoid the edge case when we only have two nodes in the list.
    If we only check `while(fast)`, we will get an error "cannot read
    property next null" telling us that fast is null.

--- 141_hasCycle.js
+++ 141_hasCycle.js
@@ -4,7 +4,9 @@ function ListNode(val) {
 };
 
 // Time Complexity: O(n)
+// Space Complexity: O(1)
 // Strategy: Hare and Tortoise
+
 // Note: Using a variable "isCycle" will cause an infinite loop.
 // Our while loop should stop ("return") immediately as soon as two pointers
 // point to the same node!
@@ -13,7 +15,9 @@ var hasCycle = (head) => {
   var slow = head;
   var fast = head;
 
-  while (slow && fast) {
+  // fast is always going to reach the end of our list before slow
+  // we should always check if fast.next is not null
+  while (fast && fast.next) { 
     slow = slow.next;
     fast = fast.next.next;
     if (slow === fast) {
@@ -26,6 +30,26 @@ var hasCycle = (head) => {
   return false
 };
 
+// Solution 2
+var hasCycle = (head) => {
+  if (!head || !head.next) return false;
+  var slow = head;
+  var fast = head.next;
+
+  // Check if we have more than two nodes
+  // If we do not check fast.next and we have only two nodes 
+  // Our fast.next.next will point to null, which will cause an error
+  while (fast && fast.next) {
+    if (slow === fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return false;
+};
+
+// Test case: 
+// 1 -> 2 -> 3 -> 4 -> 5 -> 2                                          
 var head = new ListNode(1);
 var node2 = new ListNode(2);
 var node3 = new ListNode(3);
@@ -36,4 +60,11 @@ node2.next = node3;
 node3.next = node4;
 node4.next = node5;
 node5.next = node2;
-console.log(hasCycle(head));
+
+// Edge case: only two nodes in linked list
+// 1 -> 2 
+var head2 = new ListNode(1);
+head2.next = new ListNode(2);
+
+console.log(hasCycle(head)); // return true
+console.log(hasCycle(head2)); // return false

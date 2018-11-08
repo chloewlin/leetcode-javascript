21066b7bb5353f061e19552013c33a5def1b0d4b
Chloe Lin <chloewjlin@gmail.com>
2018-11-07 23:55:01 -0800

    Add question to repo

---
+++ 215_findKthLargest.js
@@ -0,0 +1,23 @@
+// Find the kth largest element in an unsorted array. Note that it is the kth largest element 
+// in the sorted order, not the kth distinct element.
+
+// Example 1:
+// Input: [3,2,1,5,6,4] and k = 2
+// Output: 5
+
+// Example 2:
+// Input: [3,2,3,1,2,4,5,5,6] and k = 4
+// Output: 4
+
+// Note: 
+// You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+// Strategy 1: Quick Select
+var findKthLargest = function(nums, k) {
+  
+};
+
+// Strategy 2: Min Heap
+var findKthLargest = function(nums, k) {
+  
+};

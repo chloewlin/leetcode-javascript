caa3785a8ad0e7a8bade35192b35a1f725be03a7
Chloe Lin <chloewjlin@gmail.com>
2018-11-10 23:44:29 -0800

    Work on arrays to linked list conversion

--- 002_addTwoNums.js
+++ 002_addTwoNums.js
@@ -1,44 +1,74 @@
+// function LinkedList() {
+//   this.head = null;
+//   this.tail = null;
+// }
+
+// const listToNum = function(linkedList) {
+//   let arr = [];
+//   let currNode = linkedList;
+//   while (currNode) {
+//     arr.push(currNode.val);
+//     currNode = currNode.next
+//   }
+//   return parseInt(arr.reverse().join(''));
+// }
+
+// const addTwoNumbers = function(l1, l2) {
+//   const num1 = listToNum(l1); // 342 
+//   const num2 = listToNum(l2); // 465 
+//   const sum = num1 + num2;
+//   const reversedSumArr = sum.toString().split('').reverse();
+//   console.log(reversedSumArr);
+
+//   let start = null;
+//   let prev = null;
+//   for (let i = 0; i < reversedSumArr.length; i++) {
+//     let curr = new listNode(+reversedSumArr[i]);
+//     if (!start) {
+//       start = curr;
+//     }
+//     if (prev) {
+//       prev.next = curr;
+//     }
+//     prev = curr;
+//   }
+//   return start;
+// }
+
 function listNode(val) {
   this.val = val;
   this.next = null;
 }
 
-function LinkedList() {
-  this.head = null;
-  this.tail = null;
-}
-
-const listToNum = function(linkedList) {
-  let arr = [];
-  let currNode = linkedList;
-  while (currNode) {
-    arr.push(currNode.val);
-    currNode = currNode.next
+var listToString = function(linkedList) {
+  var str = '';
+  var currentNode = linkedList;
+  while (currentNode) {
+    str += currentNode.val;
+    currentNode = currentNode.next;
   }
-  return parseInt(arr.reverse().join(''));
+  return str;
 }
 
-const addTwoNumbers = function(l1, l2) {
-  const num1 = listToNum(l1); // 342 
-  const num2 = listToNum(l2); // 465 
-  const sum = num1 + num2;
-  const reversedSumArr = sum.toString().split('').reverse();
-  console.log(reversedSumArr);
+var addTwoNumbers = function(l1, l2) {
+  var str1 = +listToString(l1);
+  var str2 = +listToString(l2); 
+  var added = str1 + str2;
 
-  let start = null;
-  let prev = null;
-  for (let i = 0; i < reversedSumArr.length; i++) {
-    let curr = new listNode(+reversedSumArr[i]);
-    if (!start) {
-      start = curr;
+  var digitsArr = added.toString().split('');
+  console.log(digitsArr);
+
+  var headOfLinkedList = new listNode(+digitsArr[0]);
+  var currentNode = headOfLinkedList;
+  
+  while (currentNode) {
+    for (var i = 1; i < digitsArr.length; i++) {
+      headOfLinkedList.next = new listNode(+digitsArr[i]);
+      currentNode = currentNode.next;
     }
-    if (prev) {
-      prev.next = curr;
-    }
-    prev = curr;
   }
-  return start;
-}
+  console.log(headOfLinkedList);
+};
 
 const node1 = new listNode(2);
 node1.next = new listNode(4);

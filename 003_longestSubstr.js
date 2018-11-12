b3f05afb51739d8472994e72ad4bfdc79315836c
Chloe Lin <chloewjlin@gmail.com>
2018-11-11 22:39:51 -0800

    Add O(n) solution using sliding window strategy

--- 003_longestSubstr.js
+++ 003_longestSubstr.js
@@ -1,38 +1,57 @@
 // Given a string, find the length of the longest substring without repeating characters.
-const hasRepeat = function(str) {
-  let unique = {};
 
-  for (let i = 0; i < str.length; i++) {
-    if (!unique[str[i]]) {
-      unique[str[i]] = 1;
-    } else {
-      unique[str[i]]++; 
-    }
-  }
+// Solution 1: Brute Force
+// Time Complexity: O(n3)
+// const hasRepeat = function(str) {
+//   let unique = {};
 
-  for (var key in unique) {
-    if (unique[key] > 1) {
-      return true;
-    } 
-    // return false; // wrong place to have false
-  }
-  return false;
-} 
+//   for (let i = 0; i < str.length; i++) {
+//     if (!unique[str[i]]) {
+//       unique[str[i]] = 1;
+//     } else {
+//       unique[str[i]]++; 
+//     }
+//   }
 
+//   for (var key in unique) {
+//     if (unique[key] > 1) {
+//       return true;
+//     } 
+//   }
+//   return false;
+// } 
+
+// const lengthOfLongestSubstring = function(str) {
+//   let longest = 0;
+//   if (str.length === 0) return 0;
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = i + 1; j <= str.length; j++) {
+//       var substring = str.slice(i, j);
+//       if (!hasRepeat(substring) && substring.length > longest) {
+//         longest = substring.length;
+//       }
+//     }
+//   };
+//   return longest;
+// };
+
+// Solution 2: Sliding Window
+// Time Complexity: O(n)
 const lengthOfLongestSubstring = function(str) {
-  let combo = [];
+  let set = new Set();
+  let longestLength = 0; i = 0; j = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 1; j <= str.length; j++) {
-      let substring = str.substring(i, j);
-      if (!hasRepeat(substring)) {
-        combo.push(substring.length);
-      }
+  while (i < str.length && j < str.length) {
+    if (!set.has(str.charAt(j))) {
+      set.add(str.charAt(j++));
+      // console.log(set);
+      longestLength = Math.max(longestLength, j - i);
+    } else {
+      set.delete(str.charAt(i++));
     }
-  };
-
-  // console.log(combo);
-  return Math.max(...combo);
+  }
+  // console.log(set);
+  return longestLength;
 };
 
 console.log(lengthOfLongestSubstring('abcabcbb')); // 3 "abc"

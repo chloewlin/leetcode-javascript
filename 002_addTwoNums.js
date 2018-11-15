c7ce4c79186293f149983575d710d6b80f79c3cd
Chloe Lin <chloewjlin@gmail.com>
2018-11-14 22:06:58 -0800

    Add O(n) solution

--- 002_addTwoNums.js
+++ 002_addTwoNums.js
@@ -1,75 +1,22 @@
-// function LinkedList() {
-//   this.head = null;
-//   this.tail = null;
-// }
-
-// const listToNum = function(linkedList) {
-//   let arr = [];
-//   let currNode = linkedList;
-//   while (currNode) {
-//     arr.push(currNode.val);
-//     currNode = currNode.next
-//   }
-//   return parseInt(arr.reverse().join(''));
-// }
-
-// const addTwoNumbers = function(l1, l2) {
-//   const num1 = listToNum(l1); // 342 
-//   const num2 = listToNum(l2); // 465 
-//   const sum = num1 + num2;
-//   const reversedSumArr = sum.toString().split('').reverse();
-//   console.log(reversedSumArr);
-
-//   let start = null;
-//   let prev = null;
-//   for (let i = 0; i < reversedSumArr.length; i++) {
-//     let curr = new listNode(+reversedSumArr[i]);
-//     if (!start) {
-//       start = curr;
-//     }
-//     if (prev) {
-//       prev.next = curr;
-//     }
-//     prev = curr;
-//   }
-//   return start;
-// }
-
 function listNode(val) {
   this.val = val;
   this.next = null;
 }
 
-var listToString = function(linkedList) {
-  var str = '';
-  var currentNode = linkedList;
-  while (currentNode) {
-    str += currentNode.val;
-    currentNode = currentNode.next;
-  }
-  return str;
-}
-
 var addTwoNumbers = function(l1, l2) {
-  var str1 = +listToString(l1);
-  var str2 = +listToString(l2); 
-  var added = str1 + str2;
-
-  var digitsArr = added.toString().split('');
-  console.log(digitsArr);
-
-  var headOfLinkedList = new listNode(+digitsArr[0]);
-  var currentNode = headOfLinkedList;
-  
-  while (currentNode) {
-    for (var i = 1; i < digitsArr.length; i++) {
-      headOfLinkedList.next = new listNode(+digitsArr[i]);
-      currentNode = currentNode.next;
-    }
+  const head = new listNode();
+  for (let curr = head, carry = 0; l1 || l2 || carry; curr = curr.next) {
+      let next = new listNode((l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry);
+      carry = next.val >= 10 ? 1 : 0;
+      if (carry) next.val -= 10;
+      curr.next = next;
+      if (l1) l1 = l1.next;
+      if (l2) l2 = l2.next;
   }
-  console.log(headOfLinkedList);
+  return head.next;
 };
 
+// Should return 7 -> 0 -> 8
 const node1 = new listNode(2);
 node1.next = new listNode(4);
 node1.next.next = new listNode(3);
@@ -77,4 +24,9 @@ const node2 = new listNode(5);
 node2.next = new listNode(6);
 node2.next.next = new listNode(4);
 
-addTwoNumbers(node1, node2);
+// Should return 1 -> 8
+// const node1 = new listNode(1);
+// node1.next = new listNode(8);
+// const node2 = new listNode(0);
+
+console.log(addTwoNumbers(node1, node2));

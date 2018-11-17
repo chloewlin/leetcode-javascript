d34c842368d9f7e72034dd4d3d374b731265e3b4
Chloe Lin <chloewjlin@gmail.com>
2018-11-16 23:06:38 -0800

    Add dynamic programming solution

---
+++ 198_houseRobber.js
@@ -0,0 +1,38 @@
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, 
+// the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and 
+// it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+// Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of 
+// money you can rob tonight without alerting the police.
+
+// Example 1:
+// Input: [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+//              Total amount you can rob = 1 + 3 = 4.
+
+// Example 2:
+// Input: [2,7,9,3,1]
+// Output: 12
+// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+//              Total amount you can rob = 2 + 9 + 1 = 12.
+
+// Solution: Dynamic Programming
+const rob = function(nums) {
+  if (nums.length === 0) { return 0; }
+  if (nums.length === 1) { return nums[0]; }
+
+  const totals = [nums[0], Math.max(nums[0], nums[1])];  
+
+  for (let i = 2; i < nums.length; i++) {
+    totals.push(Math.max(nums[i] + totals[i - 2], totals[i - 1]));
+  }
+
+  return totals[totals.length - 1];
+};
+
+// console.log(rob([0])) // 0
+// console.log(rob([1,2,3,1])) // 4 (1 + 3)
+// console.log(rob([2,7,9,3,1])) // 12 (2 + 9 + 1)
+// console.log(rob([1,1,3,3,5,5])) // 9
+// console.log(rob([2,1,1,2])) // 4

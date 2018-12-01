8c51ab06a679911dbe80cfdcc43244567855ed8d
Chloe Lin <chloewjlin@gmail.com>
2018-11-30 23:11:55 -0800

    Complete with recursive solution

---
+++ 701_insertBST.js
@@ -0,0 +1,63 @@
+// Given the root node of a binary search tree (BST) and a value to be inserted into the tree, 
+// insert the value into the BST. Return the root node of the BST after the insertion. It is 
+// guaranteed that the new value does not exist in the original BST.
+
+// Note that there may exist multiple valid ways for the insertion, as long as the tree 
+// remains a BST after insertion. You can return any of them.
+
+// For example, 
+
+// Given the tree:
+//         4
+//        / \
+//       2   7
+//      / \
+//     1   3
+// And the value to insert: 5
+// You can return this binary search tree:
+
+//          4
+//        /   \
+//       2     7
+//      / \   /
+//     1   3 5
+// This tree is also valid:
+
+//          5
+//        /   \
+//       2     7
+//      / \   
+//     1   3
+//          \
+//           4
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+};
+
+// Solution 1: recursive
+var insertIntoBST = function(root, val) {
+  if (val > root.val) {
+    if (root.right) {
+      insertIntoBST(root.right, val);
+    } else {
+      root.right = new TreeNode(val);
+    }
+  } else if (val < root.val) {
+    if (root.left) {
+      insertIntoBST(root.left, val);
+    } else {
+      root.left = new TreeNode(val);
+    }
+  }
+  return root;
+};
+
+var tree = new TreeNode(4);
+tree.left = new TreeNode(2);
+tree.right = new TreeNode(7);
+tree.left.left = new TreeNode(1);
+tree.left.right = new TreeNode(3);
+
+// console.log(insertIntoBST(tree, 5));
+// console.log(insertIntoBST(tree, 0));

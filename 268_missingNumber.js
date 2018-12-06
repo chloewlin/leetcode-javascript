80f34d99781c0926c5e669852e89b51cc055d4b0
Chloe Lin <chloewjlin@gmail.com>
2018-12-05 16:31:47 -0800

    Add one-liner solution

--- 268_missingNumber.js
+++ 268_missingNumber.js
@@ -19,6 +19,11 @@ const missingNumber = (nums) => {
   return expectedSum - currentSum;
 };
 
+// One-liner
+const missingNumber = (nums) => {
+  return ((nums.length + 1) * nums.length) / 2 - nums.reduce((a, b) => a + b);
+};
+
 console.log(missingNumber([0]));
 console.log(missingNumber([3,0,1]));
 console.log(missingNumber([9,6,4,2,3,5,7,0,1]));

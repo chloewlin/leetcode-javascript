c9d27cc62c53ff3ed62426fe435f264784bb4ac0
Chloe Lin <chloewjlin@gmail.com>
2018-12-06 16:00:50 -0800

    Complete with one-liner

---
+++ 151_reverseWords.js
@@ -0,0 +1,19 @@
+// Given an input string, reverse the string word by word.
+
+// Example:  
+
+// Input: "the sky is blue",
+// Output: "blue is sky the".
+// Note:
+
+// A word is defined as a sequence of non-space characters.
+// Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+// You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+var reverseWords = (str) => str.replace(/ +/g, " ").trim().split(" ").reverse().join(" ");
+
+console.log(reverseWords("the sky is blue")); // "blue is sky the"
+console.log(reverseWords("the    sky    ")); // "sky the"
+console.log(reverseWords("  the    sky    ")); // "sky the"
+console.log(reverseWords("  the sky       ")); // "sky the"
+console.log(reverseWords("  ")); // ""

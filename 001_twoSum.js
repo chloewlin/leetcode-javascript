d7e044e4e75336ce0278a39b6044fb906b7f3cda
Chloe Lin <chloewjlin@gmail.com>
2018-12-24 23:31:43 -0800

    Add O(n) solution using for-loop

--- 001_twoSum.js
+++ 001_twoSum.js
@@ -1,17 +1,15 @@
-// Time Complexity: O(n)
-function twoSum(nums, target) {
-  let hashmap = {};
-  for (let index in nums) {
-    let difference = target - nums[index];
-    if (!hashmap[difference]) {
-      hashmap[nums[index]] = index;
-    } else {
-      return [+index, +hashmap[difference]];
-    }
-  }
-};
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+// Solution 1: Brute Force
 // Time Complexity: O(n2)
+// Space Complexity: O(n)
 function twoSum(nums, target) {
   let obj = {};
   for (let i = 0; i < nums.length; i++) {
@@ -21,3 +19,35 @@ function twoSum(nums, target) {
   }
   return obj[target];
 };
+
+// Solution 2: for-loop
+// Time Complexity: O(n) - for loop
+// Space Complexity: O(n)
+function twoSum(nums, target) {
+  let hash = {};
+  
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+
+    if (diff in hash) {
+      return [hash[diff], i];
+    }
+
+    hash[nums[i]] = i; 
+  }
+};
+
+// Solution 3: for-in loop
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+function twoSum(nums, target) {
+  let hash = {};
+  for (let i in nums) {
+    let diff = target - nums[i];
+    if (hash[diff]) return [+hash[diff], +i];
+    hash[nums[i]] = i; 
+  }
+};
+
+console.log(twoSum([2,7,11,15], 9)); // [0, 1]
+console.log(twoSum([2,11,15,7], 9)); // [0, 3]

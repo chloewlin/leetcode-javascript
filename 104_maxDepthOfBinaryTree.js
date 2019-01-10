b8f45a575f75851ca8b078e0f3bfb1a37f99058b
Chloe Lin <chloewjlin@gmail.com>
2019-01-09 20:11:06 -0800

    Add question statement and solution

--- 104_maxDepthOfBinaryTree.js
+++ 104_maxDepthOfBinaryTree.js
@@ -1,4 +1,23 @@
-// Recursive solution
+// Given a binary tree, find its maximum depth.
+
+// The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Note: A leaf is a node with no children.
+
+// Example:
+
+// Given binary tree [3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its depth = 3.
+
+
+// Recursive solutions
+// Solution 1: 
 // Time Complexity: O(n)
 var maxDepth = function(root, depth = 1) {
   if (!root) return 0;
@@ -6,15 +25,19 @@ var maxDepth = function(root, depth = 1) {
   return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
 };
 
-// Solution 2
+// Solution 2:
 // Time Complexity: O(n)
 var maxDepth = function(root, depth = 0) {
-  // if there is only one node
-  if (!root.left && !root.right) return depth + 1;
-  // terminating case
   if (!root) return depth;
-  // recursive case
   var left = maxDepth(root.left, depth + 1);
   var right = maxDepth(root.right, depth + 1);
   return Math.max(left, right);
 }
+
+// Solution 3: 
+var maxDepth = function(root) {
+  if (!root) return 0;
+  var left = maxDepth(root.left);
+  var right = maxDepth(root.right);
+  return Math.max(left, right) + 1;
+};

ac88ad1f98e1d8a9b4297c8b3233a8c30d0e892e
Chloe Lin <chloewjlin@gmail.com>
2019-01-13 23:33:00 -0800

    Add question statement and tests

--- 111_minDepthOfBinaryTree.js
+++ 111_minDepthOfBinaryTree.js
@@ -1,6 +1,37 @@
-var minDepth = function(root) {
+// Given a binary tree, find its minimum depth.
+
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+// Note: A leaf is a node with no children.
+
+// Example:
+
+// Given binary tree [3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its minimum depth = 2.
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+};
+
+const minDepth = function(root) {
   if (!root) return 0;
   if (!root.left) return 1 + minDepth(root.right);
   if (!root.right) return 1 + minDepth(root.left);
   return 1 + Math.min(minDepth(root.left), minDepth(root.right));
 };
+
+// Test
+let t = new TreeNode(3);
+t.left = new TreeNode(9);
+t.right = new TreeNode(20);
+t.right.left = new TreeNode(15);
+t.right.right = new TreeNode(7);
+
+console.log(minDepth(t)); // 2

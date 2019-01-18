c0051112180fa40092c8be33d30ec9d2066fe05d
Chloe Lin <chloewjlin@gmail.com>
2019-01-17 23:39:40 -0800

    Add problem statement

--- 005_longestPalindromeSubstr.js
+++ 005_longestPalindromeSubstr.js
@@ -1,3 +1,15 @@
+// Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+// Example 1:
+
+// Input: "babad"
+// Output: "bab"
+// Note: "aba" is also a valid answer.
+// Example 2:
+
+// Input: "cbbd"
+// Output: "bb"
+
 let isPalindrome = function(str) {
   const originalStr = str.toLowerCase();
   const reversedStr = str.toLowerCase().split('').reverse().join('');

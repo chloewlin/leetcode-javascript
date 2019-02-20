3a8eb59fca70520b7c44b19930d4537bee7711af
Chloe Lin <chloewjlin@gmail.com>
2019-02-20 01:17:54 -0800

    Update isValid with optimized O(n) solution

--- 020_validParenthesis.js
+++ 020_validParenthesis.js
@@ -1,29 +1,52 @@
-const isValid = function(s) {
-  let stack = [];
-  const openParens = {
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
+// determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+
+// Example 1:
+// Input: "()", Output: true
+
+// Example 2:
+// Input: "()[]{}", Output: true
+
+// Example 3:
+// Input: "(]", Output: false
+
+// Example 4:
+// Input: "([)]", Output: false
+
+// Example 5:
+// Input: "{[]}", Output: true
+
+// Solution: Using stack
+// Time complexity: O(n), n is the length of the input string
+// Space complexity: O(n)
+const isValid = (s) => {
+  const pairs = {
     '(': ')',
     '[': ']',
-    '{': '}',
-  }; 
-  const closingParens = {
-    ')': '(',
-    ']': '[',
-    '}': '{',
-  } 
-  for (var i = 0; i < s.length; i++) {
-    if (openParens[s[i]]) {
-      stack.push(s[i]);
-    }
-    if (closingParens[s[i]]) {
-      if (stack.pop() !== closingParens[s[i]]) {
-        return false;
-      }
+    '{': '}'
+  };
+
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (pairs[s[i]]) {
+      stack.push(pairs[s[i]]);
+    } else {
+      let popped = stack.pop();
+      if (popped !== s[i]) return false;
     }
   }
-  console.log(stack);
-  if (s.length < 2) return false;
-  return stack.length < 1 ? true: false;
+
+  // check if there is anything left in stack
+  return stack.length === 0; 
 };
+
 console.log(isValid("()")); // true
 console.log(isValid("()[]{}")); // true
 console.log(isValid("(}")); // false

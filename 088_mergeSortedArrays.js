36b1d725b59dc07ad2d20829ec80817f7d8e0a8c
Chloe Lin <chloewjlin@gmail.com>
2019-02-25 16:54:55 -0800

    Add O(n + m) time solution with annotations

--- 088_mergeSortedArrays.js
+++ 088_mergeSortedArrays.js
@@ -14,32 +14,57 @@
 // Do not return anything, modify nums1 in-place instead.
 
 // Solution 1
+// O((n + m) log (n + m)) time
+// Make a copy of both arrays, sort them, overwrite first array
 const merge = (nums1, m, nums2, n) => {
   let arr = [...nums1.slice(0, m), ...nums2.slice(0, n)];
   arr.sort((a, b) => a - b);
   arr.forEach((n, i) => nums1[i] = n);
+  // uncomment below to see the first array after merging
+  // console.log(nums1); 
 };
 
 // Solution 2
-// var merge = function(nums1, m, nums2, n) {
-//   var length = m + n - 1; 
-//   while (length) {
-//     if (n === 0) break;
-//     if (nums1[m-1] > nums2[n-1]) {
-//       nums1[length] = nums1[m-1];
-//       m--;
-//     } else {
-//       nums1[length] = nums2[n-1];
-//       n--;
-//     }
-//     length--;
-//   }
-//   console.log(nums1);
-// };
+// Copy the second array in the empty half of the first array, then sort
+const merge2 = (nums1, m, nums2, n) => {
+  if (!n) return;
+  for (let i = 0; i < n; i++) {
+    nums1[m+i] = nums2[i];
+  }
+  nums1.sort((a, b) => a - b);
+  // console.log(nums1);
+};
 
-// Tests
 var nums1 = [1,2,3,0,0,0];
 var nums2 = [2,5,6];
 
-console.log(merge(nums1, 3, nums2, 3)); //[1,2,2,3,5,6]
-console.log(merge([0], 0, [1], 1));
+// Solution 3 - best run time
+// O (n + m) time, O(1) space
+// Fill in the first array starting from the end
+var merge3 = function(nums1, m, nums2, n) {
+  // Find the starting point of the final elements
+  m--;
+  n--; 
+  while (m + n >= -1) {
+    // If a number in the first array is bigger, or nothing is left in the second array
+    // copy it over 
+    if (nums1[m] > nums2[n] || n < 0) {
+      nums1[m + n + 1] = nums1[m];
+      m--;
+    } else {
+      // fill the first array from the end
+      nums1[m + n + 1] = nums2[n];
+      n--;
+    }
+  }
+
+  // console.log(nums1);
+};
+
+// Tests
+console.log(merge(nums1, 3, nums2, 3)); // [1,2,2,3,5,6]
+console.log(merge([0], 0, [1], 1)); // [1]
+console.log(merge2(nums1, 3, nums2, 3)); // [1,2,2,3,5,6]
+console.log(merge2([0], 0, [1], 1)); // [1]
+console.log(merge3(nums1, 3, nums2, 3)); // [1,2,2,3,5,6]
+console.log(merge3([0], 0, [1], 1)); // [1]

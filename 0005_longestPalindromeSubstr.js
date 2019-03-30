e13c30c2024c113dfefb9b50d0b38a36685b9584
Chloe Lin <chloewjlin@gmail.com>
2019-03-29 22:14:27 -0700

    Complete O(n2) solution for longestPalindromeSubstr

--- 0005_longestPalindromeSubstr.js
+++ 0005_longestPalindromeSubstr.js
@@ -17,38 +17,58 @@
 //   return originalStr === reversedStr ? true : false;
 // }
 
-// O(n3) time
-let longestPalindrome = function(str) {
-  let palindromes = [];
+// Solution 1: O(n3) time
+// let longestPalindrome = function(str) {
+//   let palindromes = [];
 
-  if (str.length < 1) return "";
+//   if (str.length < 1) return "";
 
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 1; j <= str.length; j++) {
-      let substr = str.substr(i, j);
-      let length = str.substr(i, j).length;
-      if (isPalindrome(substr)) {
-        palindromes.push({substr, length});
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = 1; j <= str.length; j++) {
+//       let substr = str.substr(i, j);
+//       let length = str.substr(i, j).length;
+//       if (isPalindrome(substr)) {
+//         palindromes.push({substr, length});
+//       }
+//     }
+//   }
+
+//   let maxLength = 0;
+//   let max = null;
+//   for (let i = 0; i < palindromes.length; i++) {
+//     if (palindromes[i].length > maxLength) {
+//       maxLength = palindromes[i].length;
+//       max = palindromes[i].substr;
+//     }
+//   }
+  
+//   return max;
+// };
+
+// Solution 2: O(n2) time
+const longestPalindrome = function(s) {
+  let res = '';
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      let left = i;
+      let right = i + j;
+
+      while (s[left] && s[left] === s[right]) {
+        left--;
+        right++;
+      }
+
+      if ((right - left - 1) > res.length) {
+        res = s.substring(left + 1, right);
       }
     }
   }
 
-  let maxLength = 0;
-  let max = null;
-  for (let i = 0; i < palindromes.length; i++) {
-    if (palindromes[i].length > maxLength) {
-      maxLength = palindromes[i].length;
-      max = palindromes[i].substr;
-    }
-  }
-  
-  return max;
+  return res;
 };
 
-var longestPalindrome = function(s) {
-  
-};
-
+// Tests
 console.log(longestPalindrome("babad")); // "bab"
 console.log(longestPalindrome("cbbd")); // "bb"
 console.log(longestPalindrome("")); // ""

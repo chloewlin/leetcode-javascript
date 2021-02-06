b925cb62eaebb7fc524c23b29197782baa06088c
chloe <chloewjlin@gmail.com>
2021-02-06 00:51:56 -0800

    Update 0199_binaryTreeRightSideView.js

--- 0199_binaryTreeRightSideView.js
+++ 0199_binaryTreeRightSideView.js
@@ -62,6 +62,27 @@ const rightSideView2 = function(root) {
   return nodes;
 };
 
+// Queue
+var rightSideView3 = function(root) {
+    const queue = [];
+    const result = [];
+    if (!root) return result;
+    queue.push(root);
+    
+    while (queue.length !== 0) {
+        const size = queue.length;
+        
+        for (let i = 0; i < size; i++) {
+            const curr = queue.shift();
+            if (i === size - 1) result.push(curr.val);
+            if (curr.left) queue.push(curr.left);
+            if (curr.right) queue.push(curr.right);
+        }
+    }
+    
+    return result;
+};
+
 // Tests
 let t = new TreeNode(1);
 t.left = new TreeNode(2);
